f4003002b81845ac728162c7895893f57a30261f
Zoran <pujic.zoran@gmail.com>
2023-09-09 11:32:28 +0200

    Write Login.tsx

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,7 +1,33 @@
-const Login = () => {
-  return (
-    <div>Login page</div>
-  )
-}
+import './Login.scoped.css';
+import LoginCard from '../../components/login-card/LoginCard';
+import logo from '../../assets/logo.svg'
 
-export default Login
+const Login = () => {
+  const handleSignIn = async () => {
+    console.log("signing in action");
+  };
+  const handleSignUp = async () => {
+    console.log("signing up action");
+  };
+  const handlePasswordReset = async () => {
+    console.log("password reset clicked");
+  };
+
+  return (
+    <div className='wrapper'>
+      <LoginCard
+        onSignIn={handleSignIn}
+        onSignUp={handleSignUp}
+        onPasswordReset={handlePasswordReset}
+      />
+      <div className='animation-space'>
+        <div className='logo-holder'>
+            <img alt='logo' src={logo}/>
+            <div className='logo-text'>Taskify</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
