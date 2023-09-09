8b53e602723b3545ea304f2bd4bf5d07efc57d9d
Tino Dragojević <74689775+Damba94@users.noreply.github.com>
2023-09-09 16:55:49 +0200

    Backend/feat/database setup (#4)
    * add entities
    
    * sln fix
    
    * database setup
    
    * chore: update .gitignore
    
    * chore: adjust db schema
    
    - add migration AddTagBoardRelationship
    - set Task on delete behavior to no action
    
    * add nullable reference types
    
    * Remove unused using directives
    Improve code readability
    Fixed filename of .editorconfig file
    
    * Fix connection string issue
    enable successful update-database command
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Dario <dariomrkonjic1@gmail.com>

---
+++ backend/Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public byte[] Password { get; set; } = null!;
+        public byte[] Salt { get; set; } = null!;
+
+        public List<Assigment> Assigments { get; set; } = new List<Assigment>();
+        public List<BoardUser> BoardUsers { get; set; } = new List<BoardUser>();
+    }
+}
