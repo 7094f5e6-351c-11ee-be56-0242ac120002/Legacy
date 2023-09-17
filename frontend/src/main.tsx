a87da0585bd0fd5c062afc8d23ae9b692a910c00
Zoran Pujić <110975309+LionShareZoki@users.noreply.github.com>
2023-09-17 11:23:04 +0200

    Frontend/feat/react router prep (#5)
    * Install react router
    
    * Delete unnecessary files
    
    * Create pages
    
    * Implement BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Login from './pages/login/Login.tsx'
+import Register from './pages/register/Register.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home/Home.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
